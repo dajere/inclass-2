0fc6cdfbe538d3ab05f0228564515c0f7e8d179a
dajere newby <d.newby@setonhill.edu>
2020-02-11 11:29:03 -0500

    Initial Commit

---
+++ inclass 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  inclass 2
+//
+//  Created by dajere kyree newby on 2/11/20.
+//  Copyright © 2020 dajere kyree newby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ inclass 2Tests/inclass_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  inclass_2Tests.swift
+//  inclass 2Tests
+//
+//  Created by dajere kyree newby on 2/11/20.
+//  Copyright © 2020 dajere kyree newby. All rights reserved.
+//
+
+import XCTest
+@testable import inclass_2
+
+class inclass_2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
